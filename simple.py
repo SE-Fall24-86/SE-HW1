97fd0a431aa6b4f340f13a12782fa184a54324f0
Rohit Sriram <58294503+rohit-sram@users.noreply.github.com>
2024-08-28 01:39:47 +0000

    first commit

---
+++ simple.py
@@ -0,0 +1,24 @@
+def bubble_sort(array):
+    n = size(array)
+    
+    for i in range(n):
+        set_flag = True
+        for j in range(n - i - 1):
+            if array[j] > array[j + 1]:
+                # array[j], array[j + 1] = array[j + 1], array[j]
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+                
+                set_flag = False
+                
+        if set_flag:
+            break
+    
+    print(array)
+    # return array
+
+
+if __name__ == '__main__':    
+    A = [3, 6, 1, 5, 8, 2, 11, 7]
+    bubble_sort(A)
